a328590221c4507e5edf121e2513ca649591a94c
Samantha Freitas <84025181+sammyfreitas@users.noreply.github.com>
2022-08-02 15:16:23 -0300

    Add files via upload
    Primeira Atualização - acrescentando menu lateral com mídias sociais.

--- js/app.js
+++ js/app.js
@@ -1,50 +1,56 @@
-const squares 	= document.querySelectorAll('.square')
-const mole 		= document.querySelector('.mole')
-const timeLeft 	= document.querySelector('#time-left')
-const score 	= document.querySelector('#score')
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
 
-let recorde 	
-let result 		= 0
+let record = document.querySelector('#record')
+    //let recorde = 0
+let result = 0
 let hitPosition
 let currentTime = 60
-let timerId 	= null
+let timerId = null
 
 function randomSquare() {
-	squares.forEach(square => {
-	square.classList.remove('mole')
-  })
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
 
-	let randomSquare = squares[Math.floor(Math.random() * 16)]
-	randomSquare.classList.add('mole')
-	hitPosition = randomSquare.id
+    let randomSquare = squares[Math.floor(Math.random() * 16)]
+    randomSquare.classList.add('mole')
+    hitPosition = randomSquare.id
 }
 
 squares.forEach(square => {
-	square.addEventListener('mousedown', () => {
-		if (square.id == hitPosition) {
-			result++
-			score.textContent = result
-			recorde.textContent = recorde
-			hitPosition = null
-		}
-	})
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            record++
+            score.textContent = result
+            record.textContent = record
+            hitPosition = null
+        }
+    })
 })
 
 function moveMole() {
-	timerId = setInterval(randomSquare, 700)
+    timerId = setInterval(randomSquare, 700)
 }
 
 moveMole()
 
 function countDown() {
-	currentTime--
-	timeLeft.textContent = currentTime
+    currentTime--
+    timeLeft.textContent = currentTime
 
-	if (currentTime == 0) {
-		clearInterval(countDownTimerId)
-		clearInterval(timerId)
-		alert('GAME OVER! Sua pontuação foi: ' + result)
-	}
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('GAME OVER! Sua pontuação foi: ' + result)
+    }
 }
-record=result
+
+function restart() {
+    restart;
+}
+record = result
 let countDownTimerId = setInterval(countDown, 1000)
